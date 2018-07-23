9f17d1e49bb683d1a1ff1ada09d2b1f65ec665f0
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-23 16:47:04 +0530

    Update and rename README.md to num_odd_even.py

---
+++ num_odd_even.py
@@ -0,0 +1,11 @@
+# num_odd_even
+def main():
+    pass
+    a=list(map(int,input().split()))
+    out=a[0]+a[1]
+    if(out%2==0):
+        print("even")
+    else:
+        print("odd")
+if __name__ == '__main__':
+    main()
